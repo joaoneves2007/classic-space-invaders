c571a7bba2f96836745d585122c20a1508c43da8
João Neves <contatojotaico@gmail.com>
2024-08-05 07:54:32 -0400

    Refactored the displayGameOver and game functions

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -2,22 +2,38 @@ import EnemyController from "./EnemyController.js";
 import BulletController from "./BulletController.js";
 import Player from "./Player.js";
 
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+const instructions = document.getElementById("instructions");
+const LogosContainer = document.getElementById("logosContainer");
+const gameOverScreen = document.getElementById("gameOverScreen");
+const winScreen = document.getElementById("winScreen");
+const title = document.getElementById("title");
+const scoreDisplay = document.getElementById("scoreDisplay");
+const playButton = document.getElementById("playButton");
+const gameOverScreenButton = document.getElementById("gameOverScreenButton");
+const retryButton = document.getElementById("retryButton");
+const restartButton = document.getElementById("restartButton");
+const winScreenButton = document.getElementById("winScreenButton");
+const footer = document.getElementById("footer");
 
-canvas.width = 600;
+gameOverScreen.style.display = 'none';
+winScreen.style.display = 'none';
+scoreDisplay.style.display = 'none';
+
+canvas.width = 1024;
 canvas.height = 600;
 
 const background = new Image();
-background.src = "src/assets/images/space.png";
+background.src = "./src/assets/images/space.png";
 
-const enemyBulletController = new BulletController(canvas, 4, "red", false);
+const enemyBulletController = new BulletController(canvas, 4, "red", true);
 const playerBulletController = new BulletController(canvas, 10, "white", true);
 
 const enemyController = new EnemyController(
-    canvas, 
-    enemyBulletController, 
-    playerBulletController
+  canvas,
+  enemyBulletController,
+  playerBulletController
 );
 
 const player = new Player(canvas, 10, playerBulletController);
@@ -26,38 +42,89 @@ let isGameOver = false;
 let didWin = false;
 
 function game() {
-    checkGameOver();
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    if (!isGameOver) {
-        enemyController.draw(ctx);
-        player.draw(ctx);
-        playerBulletController.draw(ctx);
-        enemyBulletController.draw(ctx);
+  canvas.style.display = "none";
+  gameOverScreen.style.display = "none";
+  winScreen.style.display = "none";
+  title.style.display = "none";
+  scoreDisplay.style.display = "none";
+  checkGameOver();
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+  if(!isGameOver) {
+    enemyController.draw(ctx);
+    player.draw(ctx);
+    playerBulletController.draw(ctx);
+    enemyBulletController.draw(ctx);
+  } else {
+    displayGameOver();
+  }
+}
+function displayGameOver() {
+  if(isGameOver) {
+    canvas.style.display = "none";
+    title.style.display = "none";
+    scoreDisplay.style.display = "none";
+
+    if(didWin) {
+      winScreen.style.display = "flex";
+      winScreenButton.style.display = `Pontuação: ${playerScore}`;
     } else {
-        displayGameOver();
+      gameOverScreen.style.display = "flex";
+      gameOverScreenButton.style.display = `Pontuação: ${playerScore}`;
     }
+  }
+}
+
+// Funções para controlar as setas do teclado
+
+function checkGameOver() {
+  if(isGameOver) {
+    return;
+  }
+
+  if(enemyBulletController.collideWith(player) || enemyBulletController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if(enemyController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if(enemyController.enemyRows.length === 0) {
+    didWin = true;
+    isGameOver = true;
+  }
 }
 
 function displayGameOver() {
-    let text = didWin ? "Você ganhou" : "Game Over";
+  if(isGameOver) {
+    let text = didWin ? "Você Ganhou!" : "Game Over";
     let textOffset = didWin ? 5 : 3.6;
     ctx.fillStyle = "white";
     ctx.font = "35px 'Press Start 2P'";
     ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
+  }
 }
 
-function checkGameOver() {
-    if (isGameOver) {
-        return;
-    }
-    if (enemyBulletController.collideWith(player) || enemyController.collideWith(player)) {
-        isGameOver = true;
-    }
+// Função para iniciar o jogo
+function startGame() {
+  instructions.style.display = "none";
+  LogosContainer.style.display = "none";
+  footer.style.display = "none";
+  winScreen.style.display = "none";
+  gameOverScreen.style.display = "none";
 
-    if (enemyController.enemyRows.length === 0) {
-        didWin = true;
-        isGameOver = true;
-    }
+  title.style.display = "flex";
+  scoreDisplay.style.display = "flex";
+
+  canvas.style.display = "block";
+  scoreDisplay.style.display = "flex";
+
+  canva.style.display = "block";
+  initGame();
+  gameInterval = setInterval(game, 1000 / 60);
 }
 
-setInterval(game, 1000 / 60);
+playButton.addEventListener("click", startGame);
+
+initGame();
